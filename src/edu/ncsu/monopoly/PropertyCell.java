3d75841af086913e5a6e300975f3707f6ea3ac81
Miguel <admorsus3@gmail.com>
2019-12-13 22:14:14 +0100

    Extracted method

--- src/edu/ncsu/monopoly/PropertyCell.java
+++ src/edu/ncsu/monopoly/PropertyCell.java
@@ -24,14 +24,20 @@ public class PropertyCell extends Cell {
 
 	public int getRent() {
 		int rentToCharge = rent;
+		rentToCharge = monopolyGroup(rentToCharge);
+		if(numHouses > 0) {
+			rentToCharge = rent * (numHouses + 1);
+		}
+		return rentToCharge;
+	}
+
+	private int monopolyGroup (int rentToCharge)
+	{
 		String [] monopolies = propietary.getMonopolies();
 		for(int i = 0; i < monopolies.length; i++) {
 			if(monopolies[i].equals(colorGroup)) {
 				rentToCharge = rent * 2;
 			}
-		}
-		if(numHouses > 0) {
-			rentToCharge = rent * (numHouses + 1);
 		}
 		return rentToCharge;
 	}
