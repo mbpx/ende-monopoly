141b406762a27e4224f84f75e596027ebf301c8d
Miguel <admorsus3@gmail.com>
2019-12-13 22:05:46 +0100

    owner -> propietary

--- src/edu/ncsu/monopoly/Cell.java
+++ src/edu/ncsu/monopoly/Cell.java
@@ -3,14 +3,14 @@ package edu.ncsu.monopoly;
 public abstract class Cell {
 	private boolean available = true;
 	private String name;
-	protected Player owner;
+	protected Player propietary;
 
 	public String getName() {
 		return name;
 	}
 
 	public Player getOwner() {
-		return owner;
+		return propietary;
 	}
 	
 	public int getPrice() {
@@ -32,7 +32,7 @@ public abstract class Cell {
 	}
 
 	public void setOwner(Player owner) {
-		this.owner = owner;
+		this.propietary = owner;
 	}
     
     public String toString() {
